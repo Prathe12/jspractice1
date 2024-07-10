0122b57fa033baaee469e59524d0d0ea2bea3c53
Prathe12 <rpratheba28@gmail.com>
2024-07-10 14:29:47 +0530

    update

--- 04_JS_Functions/Ch4_pgm_11.js
+++ 04_JS_Functions/Ch4_pgm_11.js
@@ -41,6 +41,44 @@ showMovieInfo();
 movie = movie3;
 showMovieInfo();
 
+var question1, question2, question3, question, showQuizQuestion;
+
+question1 = {
+    text: "What is the capital of India?",
+    options: ["A: Mumbai", "B: Kolkata", "C: New Delhi", "D: Rajasthan"],
+    correctAnswer: "C"
+};
+
+question2 = {
+    text: "What is the chemical symbol for Gold?",
+    options: ["A: Gd", "B: Go", "C: Ag", "D: Au"],
+    correctAnswer: "D"
+};
+
+question3 = {
+    text: "What is the tallest mountain in the world?",
+    options: ["A: K2", "B: Mount Everest", "C: Mount Kilimanjaro", "D: Denali"],
+    correctAnswer: "B"
+};
+
+showQuizQuestion = function () {
+    console.log("Question: " + question.text);
+    console.log("Options: ");
+    for (var i = 0; i < question.options.length; i++) {
+        console.log(question.options[i]);
+    }
+    console.log("Correct Answer: " + question.correctAnswer);
+    console.log("------------------------------");
+};
+
+question = question1;
+showQuizQuestion();
+
+question = question2;
+showQuizQuestion();
+
+question = question3;
+showQuizQuestion();
 
 
 /* Further Adventures
