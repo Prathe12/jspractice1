3d69d18b8e20555054e29851b7237a88308a9042
Prathe12 <rpratheba28@gmail.com>
2024-07-10 20:25:18 +0530

    update

--- 05_JS_Arguments/Ch5_pgm_09.js
+++ 05_JS_Arguments/Ch5_pgm_09.js
@@ -3,11 +3,18 @@
 var showPlayerHealth;
 
 showPlayerHealth = function (playerName, playerHealth) {
-    console.log(playerName + " has health " + playerHealth);
+    var healthInfo;
+    healthInfo = playerName + ": health " + playerHealth;
+console.log(healthInfo);
 };
+
+
+
+
 
 showPlayerHealth("Kandra", 50);
 showPlayerHealth("Dax", 40);
+showPlayerHealth("Tessa", 70);
 
 
 
