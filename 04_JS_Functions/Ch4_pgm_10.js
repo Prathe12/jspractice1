b6e2f75648bcc92ea5c1f4ebaa7047ac4562cbff
Prathe12 <rpratheba28@gmail.com>
2024-07-10 14:25:58 +0530

    update

--- 04_JS_Functions/Ch4_pgm_10.js
+++ 04_JS_Functions/Ch4_pgm_10.js
@@ -5,7 +5,7 @@ var showMovieInfo;
 var movie;
 var movie2 = {
     title: "Inception",
-    actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Elliot Page"],
+    actors: ["Leonardo DiCaprio", "Joseph Gordon-Levitt", "Elliot Page", "Cillian Murphy"],
     directors: ["Christopher Nolan"]
 }
 
