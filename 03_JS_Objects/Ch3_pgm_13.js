7d40cee81efac712542be1ce0048df46eea92380
Prathe12 <rpratheba28@gmail.com>
2024-07-10 13:00:44 +0530

    update

--- 03_JS_Objects/Ch3_pgm_13.js
+++ 03_JS_Objects/Ch3_pgm_13.js
@@ -9,7 +9,7 @@ var location = {
     "longitude" : -122.418,
     "elevation" : 47.000
   };
-  console.log(location.city + " in " + location.state + " in " + location.country)
+  console.log(location.city + " in " + location.state + " in " + location.country + "latitude of" + location.latitude);
   
   
   /* Further Adventures
