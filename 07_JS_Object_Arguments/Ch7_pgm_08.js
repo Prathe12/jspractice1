4c7aa3ca3112dbe65f50c7d99c3bf133c8867942
Prathe12 <rpratheba28@gmail.com>
2024-07-11 21:27:29 +0530

    update

--- 07_JS_Object_Arguments/Ch7_pgm_08.js
+++ 07_JS_Object_Arguments/Ch7_pgm_08.js
@@ -3,6 +3,12 @@
 var message = "We choose to go to the Moon!";
 
 console.log(message.substr(3, 12));
+console.log(message.substr(21, 5));
+console.log(message.substr(13, 9,));
+console.log(message.substr(0, 9));
+console.log(message.substr(3));
+console.log(message.substr(-5));
+console.log(message.substr(-8, 3));
 
 
 
