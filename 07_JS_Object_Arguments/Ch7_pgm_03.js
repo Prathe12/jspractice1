036330359045a392a11f130a1015fdfe680d6326
Prathe12 <rpratheba28@gmail.com>
2024-07-11 20:52:05 +0530

    update

--- 07_JS_Object_Arguments/Ch7_pgm_03.js
+++ 07_JS_Object_Arguments/Ch7_pgm_03.js
@@ -25,7 +25,30 @@ planet2 = buildPlanet("Neptune", 8, "Ice Giant", 24622, 4);
 console.log(getPlanetInfo(planet1));
 console.log(getPlanetInfo(planet2));
 
+var buildCar;
+var getCarInfo;
+var car1;
+var car2;
 
+buildCar = function (make, color, type, model, year) {
+    return{
+        make: make,
+        color: color,
+        type: type,
+        model: model,
+        year: year
+    };
+
+};
+
+getCarInfo = function(car) {
+    return car.make.toUpperCase() + ":car " + car.color;
+};
+car1 = buildCar("Tata", "Black", "Coupe", "Tigor" , 2024);
+car2 = buildCar("Toyota", "white", "sedan", "Innova", 2019);
+
+console.log(getCarInfo(car1));
+console.log(getCarInfo(car2));
 
 /* Further Adventures
  *
