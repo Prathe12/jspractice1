ef016e012936347afb51b1db05316a45fdcbff92
Prathe12 <rpratheba28@gmail.com>
2024-07-11 21:10:57 +0530

    update

--- 07_JS_Object_Arguments/Ch7_pgm_06.js
+++ 07_JS_Object_Arguments/Ch7_pgm_06.js
@@ -7,9 +7,53 @@ var line = function (lineLength) {
     return line.substr(0, lineLength);
   };
   
-  console.log(line(30));
-  console.log(line(40));
-  console.log(line(50));
+  var spaces = function (spaceLength) {
+    var spaces = "                                        "; 
+    spaceLength = Math.max(0, spaceLength);
+    spaceLength = Math.min(40, spaceLength);
+    return spaces.substr(0, spaceLength);
+};
+
+var emptyBox = function (width) {
+    width = Math.max(2, width);
+    width = Math.min(40, width); 
+
+    var topAndBottom = line(width);
+    var middle = "=" + spaces(width - 2) + "=";
+
+    console.log(topAndBottom);
+    for (var i = 0; i < 3; i++) {
+        console.log(middle);
+    }
+    console.log(topAndBottom);
+};
+
+    for (var i = -20; i <= 60; i += 10) {
+        console.log("line(" + i + "):");
+        console.log(line(i));
+        console.log();
+}
+
+
+        console.log("spaces(10):");
+        console.log('"' + spaces(10) + '"');
+        console.log();
+
+        console.log("spaces(50):");
+        console.log('"' + spaces(50) + '"');
+        console.log();
+
+
+        console.log("emptyBox(12):");
+        emptyBox(12);
+        console.log();
+
+        console.log("emptyBox(45):");
+        emptyBox(45);
+        console.log();
+        console.log(line(30));
+        console.log(line(40));
+        console.log(line(50));
   
   
   
