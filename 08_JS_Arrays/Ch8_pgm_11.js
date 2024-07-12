9eb0e9ff87e351b6b6e377a7079b3197c64f6903
Prathe12 <rpratheba28@gmail.com>
2024-07-12 15:34:00 +0530

    update

--- 08_JS_Arrays/Ch8_pgm_11.js
+++ 08_JS_Arrays/Ch8_pgm_11.js
@@ -16,6 +16,44 @@ var numOfEach = [ 2, 1, 5, 2 ];
 console.log("The total cost is $" + getTotalBill(costs, numOfEach));
 
 
+var costs = [1.99, 4.95, 2.50, 9.87, 3.25];
+var numOfEach = [2, 1, 5, 2, 3];
+
+
+var getTotalBill = function (itemCosts, itemCounts) {
+    var total = 0;
+
+    itemCounts.forEach(function (count, i) {
+        total += count * itemCosts[i];
+    });
+
+    return total;
+};
+
+console.log("The total cost is $" + getTotalBill(costs, numOfEach));
+
+
+var items = [
+    { cost: 1.99, numberBought: 2 },
+    { cost: 4.95, numberBought: 1 },
+    { cost: 2.50, numberBought: 5 },
+    { cost: 9.87, numberBought: 2 },
+    { cost: 3.25, numberBought: 3 }
+];
+
+
+var getTotalBillFromItems = function (items) {
+    var total = 0;
+
+    items.forEach(function (item) {
+        total += item.cost * item.numberBought;
+    });
+
+    return total;
+};
+
+console.log("The total cost is $" + getTotalBillFromItems(items));
+
 
 /* Further Adventures
  *
