9ab21f552987dfe4371cbe3234475a4ffb3a2b5f
Prathe12 <rpratheba28@gmail.com>
2024-07-12 15:03:29 +0530

    update

--- 08_JS_Arrays/Ch8_pgm_07.js
+++ 08_JS_Arrays/Ch8_pgm_07.js
@@ -6,8 +6,35 @@ var showInfo;
 items = [
   "The Pyramids",
   "The Grand Canyon",
-  "Bondi Beach"
+  "Bondi Beach",
+  "The Great Wall of China",
+  "India Gate"
 ];
+
+items.push("EiffelTower");
+items.push("Machu Picchu");
+items[4] = "Christ the Redeemer";
+items[5] = "The Taj Mahal";
+function showInfo() {
+  for (let i = 0; i < items.length; i++) {
+      if (items[i]) {
+          console.log(`${items[i]} (Length: ${items[i].length} characters)`);
+      }
+  }
+}
+function totalNumberOfLetters() {
+  let totalLetters = 0;
+  for (let i = 0; i < items.length; i++) {
+      if (items[i]) {
+          totalLetters += items[i].length;
+      }
+  }
+  return totalLetters;
+}
+
+
+showInfo();
+console.log(`Total number of letters: ${totalNumberOfLetters()}`);
 
 showInfo = function (itemToShow) {
     console.log(itemToShow);
