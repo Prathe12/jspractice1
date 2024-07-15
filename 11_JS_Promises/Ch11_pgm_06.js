cf73939ca415fbbd5bd7545fed4fe013e58890d8
Prathe12 <rpratheba28@gmail.com>
2024-07-15 14:37:14 +0530

    update

--- 11_JS_Promises/Ch11_pgm_06.js
+++ 11_JS_Promises/Ch11_pgm_06.js
@@ -1,4 +1,16 @@
-const countriesAPI = 'https://restcountries.com/v2/all'
+
 const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+const fetchCatNames = async () => {
+    try {
+      const response = await fetch(catsAPI)
+      const cats = await response.json()
+      const catNames = cats.map(cat => cat.name)
+      console.log('Cat Names:', catNames)
+    } catch (error) {
+      console.error('Error fetching cat data:', error)
+    }
+  }
+  fetchCatNames()
+  
 
 // Print out all the cat names in to catNames variable.
