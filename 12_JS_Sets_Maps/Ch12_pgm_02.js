7f0817d6dbe791095b3a62e0a0187316beb4dee6
Prathe12 <rpratheba28@gmail.com>
2024-07-15 15:38:47 +0530

    update

--- 12_JS_Sets_Maps/Ch12_pgm_02.js
+++ 12_JS_Sets_Maps/Ch12_pgm_02.js
@@ -2,6 +2,20 @@ const a = [4, 5, 8, 9]
 const b = [3, 4, 5, 7]
 const countries = ['Finland', 'Sweden', 'Norway']
 
+const setA = new Set(a)
+const setB = new Set(b)
+
+const union = new Set([...setA, ...setB])
+console.log('Union:', [...union])
+
+const intersection = new Set([...setA].filter(x => setB.has(x)))
+console.log('Interaction:', [...intersection])
+
+const difference = new Set([...setA].filter(x => !setB.has(x)))
+console.log('Difference (a with b):', [...difference])
+
+
+
 // Find a union b
 // Find a intersection b
 // Find a with b
