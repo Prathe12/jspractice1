7655b11e15913878180d75d223a442bc41f47fcc
Prathe12 <rpratheba28@gmail.com>
2024-07-15 15:24:45 +0530

    update

--- 12_JS_Sets_Maps/Ch12_pgm_01.js
+++ 12_JS_Sets_Maps/Ch12_pgm_01.js
@@ -1,7 +1,30 @@
 const a = [4, 5, 8, 9]
 const b = [3, 4, 5, 7]
 const countries = ['Finland', 'Sweden', 'Norway']
+const emptySet = new Set()
+console.log(emptySet)
 
+ const setWithNumbers = new Set()
+ for (let i = 0; i <= 10; i++) {
+  setWithNumbers.add(i)
+}
+console.log(setWithNumbers)
+
+setWithNumbers.delete(5)
+console.log(setWithNumbers)
+
+setWithNumbers.clear()
+console.log(setWithNumbers)
+
+const stringArray = ['a', 'b', 'c', 'd', 'e']
+const stringSet = new Set(stringArray)
+console.log(stringSet)
+
+const countriesMap = new Map()
+countries.forEach(country =>{
+    countriesMap.set(country, country.length)
+})
+console.log(countriesMap)
 
 // create an empty set
 // Create a set containing 0 to 10 using loop
